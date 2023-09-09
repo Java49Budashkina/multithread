8673fd2b1d21e5fc78dee6f330cea29f1df04318
Anna <annbudash@mail.ru>
2023-09-09 13:54:42 +0300

    add AtomicInteger

--- src/telran/multithreading/Cockroach.java
+++ src/telran/multithreading/Cockroach.java
@@ -1,6 +1,7 @@
 package telran.multithreading;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Cockroach  extends Thread {
 	public Cockroach(int number, int numCycle) {
@@ -10,13 +11,13 @@ public class Cockroach  extends Thread {
 	private  int number;
 	private int numCycle;
 	
-	static public int winner;
+	static public AtomicInteger winner = new AtomicInteger();
 	
 	
 	@Override
 	public void run() {
 		int i=0;
-		while (winner == 0)
+		while (winner.get() == 0)
 		{
 			System.out.printf("thread %d \n", number);
 			if( i< numCycle) {
@@ -30,7 +31,8 @@ public class Cockroach  extends Thread {
 				}
 			}
 			else {
-				winner = winner == 0 ? number : winner;
+				if(winner.get() == 0)
+					winner.set(number);
 			}				
 		}
 		System.out.printf("thread %d ended  i=%d (from %d) winner = %d\n", number, i,numCycle, winner);
