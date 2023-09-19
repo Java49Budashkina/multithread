7bd9d894716f37b8e7344ec3ee208549c75d9a5e
Anna <annbudash@mail.ru>
2023-09-19 23:40:08 +0300

    hw45 - race results table

---
+++ src/hw45/Race.java
@@ -0,0 +1,41 @@
+package hw45;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Race {
+	private int distance;
+	private int minSleep;
+	private int maxSleep;
+	private ArrayList<Runner> resultsTable;
+	private Instant startTime;
+	public Race(int distance, int minSleep, int maxSleep, ArrayList<Runner> resultsTable, Instant startTime) {
+		this.distance = distance;
+		this.minSleep = minSleep;
+		this.maxSleep = maxSleep;
+		this.resultsTable = resultsTable;
+		this.startTime = startTime;
+	}
+	public ArrayList<Runner> getResultsTable() {
+		return resultsTable;
+	}
+	public Instant getStartTime() {
+		return startTime;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public int getMinSleep() {
+		return minSleep;
+	}
+	public int getMaxSleep() {
+		return maxSleep;
+	}
+	
+	public int getRandomSleep() {
+		return new Random().nextInt(minSleep, maxSleep);
+	}
+
+	
+}
---
+++ src/hw45/RaceAppl.java
@@ -0,0 +1,66 @@
+package hw45;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class RaceAppl {
+	static int minDistance = 10;  
+	static int maxDistance = 100;
+	static int minSleep = 2;  
+	static int maxSleep = 5;
+	private static final int MAX_THREADS = 10;
+	private static final int MIN_THREADS = 3;
+	
+	public static void main(String[] args) {
+		int distance = new Random().nextInt(minDistance, maxDistance);
+		int nRunners = new Random().nextInt(MIN_THREADS, MAX_THREADS );
+		Race race = new Race(distance, minSleep, maxSleep, new ArrayList<Runner>(), Instant.now());
+		Runner[] runners = new Runner[nRunners];
+		
+		startRunners(runners,race);
+		joinRunners(runners);
+		displayResult( race);
+		
+		
+	}
+
+
+	
+	private static void joinRunners(Runner[] runners) {
+		IntStream.range(0, runners.length).forEach(i -> {
+			try {
+				   runners[i].join();
+			   }
+			   catch(InterruptedException e) {
+				   throw new IllegalStateException();
+			   }
+		});
+	}
+
+	private static void startRunners(Runner[] runners, Race race) {
+		IntStream.range(0, runners.length).forEach(i  -> {
+		   runners[i] = new Runner(race, i+1);
+		    runners[i].start();
+		});
+		
+	}
+	private static void displayResult(Race race) {
+		ArrayList<Runner> tabl = race.getResultsTable();
+
+		IntStream.range(0, tabl.size()).forEach(i  -> {
+			System.out.printf("%3d\t number %7d\t time \t%d ms\n", i+1, tabl.get(i).getRunnerId(),
+					ChronoUnit.MILLIS.between(race.getStartTime(), tabl.get(i).getFinsishTime()));
+			});
+	}
+	
+}
+		
+	
+	
+
+	
+	
+
